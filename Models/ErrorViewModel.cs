18fdbd2b4bfa6c9db3a10a356da9bef0f2777581
Jobayer Alam <sheikhnayamul123@gmail.com>
2025-03-14 17:49:14 +0600

    done

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomValidationAttributeAndExtensionMethod.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
